8d6a41e6072f7d9aa1305b26c0699410a7f7aa7a
Stepan <stepan.dubrov@gmail.com>
2023-11-19 21:59:36 +0300

    Initial commit

---
+++ MainView.axaml.cs
@@ -0,0 +1,36 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace Tg_bot_GUI
+{
+    public partial class MainView : UserControl
+    {
+        private TextBox _inputTextBox;
+
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+            _inputTextBox = this.FindControl<TextBox>("InputTextBox");
+        }
+
+        private void OnConfirmButtonClick(object sender, RoutedEventArgs e)
+        {
+            var enteredText = _inputTextBox?.Text;
+
+            // Создаем экземпляр ResultView
+            var resultView = new ResultView(this);
+
+            // Устанавливаем текст в ResultView
+            resultView.SetResultText($"Введенный текст: {enteredText}");
+
+            // Заменяем текущий UserControl на ResultView
+            this.Content = resultView;
+        }
+    }
+}
